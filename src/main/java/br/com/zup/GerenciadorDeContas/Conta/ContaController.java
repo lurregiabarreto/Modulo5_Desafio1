f12bb4caf9deefbedd4c5e898db7340267b23065
Lurrégia Barreto <luhhbarreto@gmail.com>
2021-11-10 15:17:59 -0300

    criar classe controller e anotações

---
+++ src/main/java/br/com/zup/GerenciadorDeContas/Conta/ContaController.java
@@ -0,0 +1,12 @@
+package br.com.zup.GerenciadorDeContas.Conta;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/contas")
+
+public class ContaController {
+
+}
