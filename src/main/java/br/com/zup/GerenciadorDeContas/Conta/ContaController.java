1cf3e66857c435a2c705db699ea040154d15ca99
Lurrégia Barreto <luhhbarreto@gmail.com>
2021-11-11 14:38:55 -0300

    modelar metodo para buscar contas cadastradas

--- src/main/java/br/com/zup/GerenciadorDeContas/Conta/ContaController.java
+++ src/main/java/br/com/zup/GerenciadorDeContas/Conta/ContaController.java
@@ -1,11 +1,15 @@
 package br.com.zup.GerenciadorDeContas.Conta;
 
 
+import br.com.zup.GerenciadorDeContas.dtos.ResumoContaDTO;
 import br.com.zup.GerenciadorDeContas.dtos.SaidaContaDTO;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/contas")
@@ -24,5 +28,17 @@ public class ContaController {
         SaidaContaDTO respostaCadastroDTO = modelMapper.map(contaService.cadastrarConta(conta), SaidaContaDTO.class);
 
         return respostaCadastroDTO;
+
+    }
+
+    @GetMapping
+    public List<ResumoContaDTO> buscarContasCadastradas() {
+        List<ResumoContaDTO> listaResumo = new ArrayList<>();
+
+        for (Conta conta : contaService.buscarContasCadastradas()) {
+            ResumoContaDTO resumo = modelMapper.map(conta, ResumoContaDTO.class);
+            listaResumo.add(resumo);
+        }
+        return listaResumo;
     }
 }
