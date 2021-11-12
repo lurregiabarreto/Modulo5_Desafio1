2f8e06d07333012a78d7565d055fd952c132cd5e
Lurrégia Barreto <luhhbarreto@gmail.com>
2021-11-12 09:53:28 -0300

    adiconar anotações para validações

--- src/main/java/br/com/zup/GerenciadorDeContas/dtos/ContaDTO.java
+++ src/main/java/br/com/zup/GerenciadorDeContas/dtos/ContaDTO.java
@@ -2,12 +2,22 @@ package br.com.zup.GerenciadorDeContas.dtos;
 
 import br.com.zup.GerenciadorDeContas.enuns.Tipo;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 public class ContaDTO {
+    @Size(min=3, message = "Nome deve ter acima de 3 letras ")
+    @NotNull
+    @NotEmpty
     private String nome;
+    @Min(value = 1, message = "O valor é obrigatório e deve ser maior ou igual a R$ 1.00")
     private double valor;
+    @NotNull(message = "Tipo é obrigatório")
     private Tipo tipo;
+    @NotNull
     private LocalDate dataDeVencimento;
 
     public ContaDTO() {
