de52b730fe2f74a5533f379bdb8fe93ee1622098
Lurrégia Barreto <luhhbarreto@gmail.com>
2021-11-12 15:46:31 -0300

    criar query para valor aproximado

--- src/main/java/br/com/zup/GerenciadorDeContas/Conta/ContaRepository.java
+++ src/main/java/br/com/zup/GerenciadorDeContas/Conta/ContaRepository.java
@@ -1,7 +1,22 @@
 package br.com.zup.GerenciadorDeContas.Conta;
 
+import br.com.zup.GerenciadorDeContas.enuns.Status;
+import br.com.zup.GerenciadorDeContas.enuns.Tipo;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ContaRepository  extends CrudRepository<Conta, Integer> {
+import java.util.List;
 
+public interface ContaRepository extends CrudRepository<Conta, Integer> {
+
+    List<Conta> findAllByStatus(Status status);
+
+    List<Conta> findAllByTipo(Tipo tipo);
+
+
+    @Query(value = "SELECT * FROM contas WHERE valor BETWEEN :valor*0.85 AND :valor*1.15", nativeQuery = true)
+    List<Conta> findAllByValorAproximado(double valor);
 }
+
+
+
