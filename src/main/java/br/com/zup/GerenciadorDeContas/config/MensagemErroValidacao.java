ac1f2ae91552d8a20d1a9ecb771975ed3ae764ef
Lurrégia Barreto <luhhbarreto@gmail.com>
2021-11-12 07:04:18 -0300

    criar classe de mensagem de erro na validação

--- src/main/java/br/com/zup/GerenciadorDeContas/config/MensagemErroValidacao.java
+++ src/main/java/br/com/zup/GerenciadorDeContas/config/MensagemErroValidacao.java
@@ -1,2 +1,20 @@
-package br.com.zup.GerenciadorDeContas.config;public class MensagemErroValidacao {
+package br.com.zup.GerenciadorDeContas.config;
+
+public class MensagemErroValidacao {
+    private String mensagem;
+    private String campo;
+
+    public MensagemErroValidacao(String mensagem, String campo) {
+        this.mensagem = mensagem;
+        this.campo = campo;
+    }
+
+    public String getCampo() {
+        return campo;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
 }
+
